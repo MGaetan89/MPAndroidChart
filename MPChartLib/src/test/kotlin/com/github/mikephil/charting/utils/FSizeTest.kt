ae4e032cb1a09c26b49e9a770250fdbda0ffb18c
Gaëtan Muller <m.gaetan89@gmail.com>
2018-03-08 23:06:40 +0100

    Update tests in the `utils` package

--- MPChartLib/src/test/kotlin/com/github/mikephil/charting/utils/FSizeTest.kt
+++ MPChartLib/src/test/kotlin/com/github/mikephil/charting/utils/FSizeTest.kt
@@ -3,16 +3,22 @@ package com.github.mikephil.charting.utils
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
+@Suppress("ReplaceCallWithComparison")
 class FSizeTest {
 	@Test
 	fun fSize() {
 		val size = FSize(1f, 2f)
 		assertThat(size.height).isEqualTo(2f)
 		assertThat(size.width).isEqualTo(1f)
+		assertThat(size.equals(null)).isFalse()
+		assertThat(size.equals(42)).isFalse()
+		assertThat(size == size).isTrue()
 		assertThat(size == FSize(0f, 0f)).isFalse()
+		assertThat(size == FSize(1f, 1f)).isFalse()
 		assertThat(size == FSize(1f, 2f)).isTrue()
 		assertThat(size == FSize(2f, 1f)).isFalse()
 		assertThat(size == FSize(2f, 2f)).isFalse()
+		assertThat(size.hashCode()).isEqualTo(2139095040)
 		assertThat(size.toString()).isEqualTo("1.0x2.0")
 
 		val other = size.instantiate() as FSize
@@ -25,10 +31,15 @@ class FSizeTest {
 		val size = FSize()
 		assertThat(size.height).isEqualTo(0f)
 		assertThat(size.width).isEqualTo(0f)
+		assertThat(size.equals(null)).isFalse()
+		assertThat(size.equals(42)).isFalse()
+		assertThat(size == size).isTrue()
 		assertThat(size == FSize(0f, 0f)).isTrue()
+		assertThat(size == FSize(0f, 1f)).isFalse()
 		assertThat(size == FSize(1f, 2f)).isFalse()
 		assertThat(size == FSize(2f, 1f)).isFalse()
 		assertThat(size == FSize(2f, 2f)).isFalse()
+		assertThat(size.hashCode()).isEqualTo(0)
 		assertThat(size.toString()).isEqualTo("0.0x0.0")
 
 		val other = size.instantiate() as FSize
