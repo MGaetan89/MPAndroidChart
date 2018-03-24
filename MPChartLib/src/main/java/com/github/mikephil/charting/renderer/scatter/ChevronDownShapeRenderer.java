8f23ab70182eed7d5ebda49bf1b38cb645436045
Gaëtan Muller <m.gaetan89@gmail.com>
2018-03-24 19:25:55 +0100

    Format classes in the scatter package

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/scatter/ChevronDownShapeRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/scatter/ChevronDownShapeRenderer.java
@@ -9,33 +9,19 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 
 /**
  * Created by wajdic on 15/06/2016.
- * Created at Time 09:08
  */
-public class ChevronDownShapeRenderer implements IShapeRenderer
-{
-
-
+public class ChevronDownShapeRenderer implements IShapeRenderer {
     @Override
-    public void renderShape(Canvas c, IScatterDataSet dataSet, ViewPortHandler viewPortHandler,
-                     float posX, float posY, Paint renderPaint) {
-
+    public void renderShape(
+            Canvas canvas, IScatterDataSet dataSet, ViewPortHandler viewPortHandler,
+            float posX, float posY, Paint renderPaint
+    ) {
         final float shapeHalf = dataSet.getScatterShapeSize() / 2f;
 
         renderPaint.setStyle(Paint.Style.STROKE);
         renderPaint.setStrokeWidth(Utils.convertDpToPixel(1f));
 
-        c.drawLine(
-                posX,
-                posY + (2 * shapeHalf),
-                posX + (2 * shapeHalf),
-                posY,
-                renderPaint);
-
-        c.drawLine(
-                posX,
-                posY + (2 * shapeHalf),
-                posX - (2 * shapeHalf),
-                posY,
-                renderPaint);
+        canvas.drawLine(posX, posY + (2f * shapeHalf), posX + (2f * shapeHalf), posY, renderPaint);
+        canvas.drawLine(posX, posY + (2f * shapeHalf), posX - (2f * shapeHalf), posY, renderPaint);
     }
 }
--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/scatter/SquareShapeRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/scatter/SquareShapeRenderer.java
@@ -10,54 +10,51 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 
 /**
  * Created by wajdic on 15/06/2016.
- * Created at Time 09:08
  */
-public class SquareShapeRenderer implements IShapeRenderer
-{
-
-
+public class SquareShapeRenderer implements IShapeRenderer {
     @Override
-    public void renderShape(Canvas c, IScatterDataSet dataSet, ViewPortHandler viewPortHandler,
-                            float posX, float posY, Paint renderPaint) {
-
+    public void renderShape(
+            Canvas canvas, IScatterDataSet dataSet, ViewPortHandler viewPortHandler,
+            float posX, float posY, Paint renderPaint
+    ) {
         final float shapeSize = dataSet.getScatterShapeSize();
         final float shapeHalf = shapeSize / 2f;
         final float shapeHoleSizeHalf = Utils.convertDpToPixel(dataSet.getScatterShapeHoleRadius());
-        final float shapeHoleSize = shapeHoleSizeHalf * 2.f;
-        final float shapeStrokeSize = (shapeSize - shapeHoleSize) / 2.f;
-        final float shapeStrokeSizeHalf = shapeStrokeSize / 2.f;
-
+        final float shapeHoleSize = shapeHoleSizeHalf * 2f;
+        final float shapeStrokeSize = (shapeSize - shapeHoleSize) / 2f;
+        final float shapeStrokeSizeHalf = shapeStrokeSize / 2f;
         final int shapeHoleColor = dataSet.getScatterShapeHoleColor();
 
-        if (shapeSize > 0.0) {
+        if (shapeSize > 0f) {
             renderPaint.setStyle(Paint.Style.STROKE);
             renderPaint.setStrokeWidth(shapeStrokeSize);
 
-            c.drawRect(posX - shapeHoleSizeHalf - shapeStrokeSizeHalf,
+            canvas.drawRect(
+                    posX - shapeHoleSizeHalf - shapeStrokeSizeHalf,
                     posY - shapeHoleSizeHalf - shapeStrokeSizeHalf,
                     posX + shapeHoleSizeHalf + shapeStrokeSizeHalf,
                     posY + shapeHoleSizeHalf + shapeStrokeSizeHalf,
-                    renderPaint);
+                    renderPaint
+            );
 
             if (shapeHoleColor != ColorTemplate.COLOR_NONE) {
                 renderPaint.setStyle(Paint.Style.FILL);
-
                 renderPaint.setColor(shapeHoleColor);
-                c.drawRect(posX - shapeHoleSizeHalf,
-                        posY - shapeHoleSizeHalf,
-                        posX + shapeHoleSizeHalf,
-                        posY + shapeHoleSizeHalf,
-                        renderPaint);
-            }
 
+                canvas.drawRect(
+                        posX - shapeHoleSizeHalf, posY - shapeHoleSizeHalf,
+                        posX + shapeHoleSizeHalf, posY + shapeHoleSizeHalf,
+                        renderPaint
+                );
+            }
         } else {
             renderPaint.setStyle(Paint.Style.FILL);
 
-            c.drawRect(posX - shapeHalf,
-                    posY - shapeHalf,
-                    posX + shapeHalf,
-                    posY + shapeHalf,
-                    renderPaint);
+            canvas.drawRect(
+                    posX - shapeHalf, posY - shapeHalf,
+                    posX + shapeHalf, posY + shapeHalf,
+                    renderPaint
+            );
         }
     }
 }
